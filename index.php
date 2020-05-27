639930ae48fd7ae5d53f0de3b7edfaa4e71b77e9
rochouhan <ro.chouhan@gmail.com>
2020-05-27 15:58:25 -0500

    55. Navigation links color change on hover

--- index.php
+++ index.php
@@ -14,73 +14,105 @@ if (isset($_SESSION['userLoggedIn'])) {
 </head>
 
 <body>
-    <div id="nowPlayingBarContainer">
-        <div id="nowPlayingBar">
-            <div id="nowPlayingLeft">
-                <div class="content playerControls">
-                    <span class="albumLink">
-                        <img class="albumArtwork" src="https://lh3.googleusercontent.com/proxy/fVr-noQ5XeedHxwiECY_vvOJgzLGP2m2o54ekSrHSCIF44HWP-Chg_vHv3CYFKpY8wJv3lc68OCh8knwSTSmExOC" alt="">
-                    </span>
+    <div id="mainContainer">
+        <div id="topContainer">
+            <div id="navBarContainer">
+                <nav class="navBar">
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/icons/logo.png" alt="">
+                    </a>
 
-                    <div class="trackInfo">
-                        <span class="trackName">
-                            <span>Forever</span>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">
+                                Search
+                                <img src="assets/images/icons/search.png" alt="Search" class="icon">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="yourmusic.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Rohit Chouhan</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div id="nowPlayingBarContainer">
+            <div id="nowPlayingBar">
+                <div id="nowPlayingLeft">
+                    <div class="content playerControls">
+                        <span class="albumLink">
+                            <img class="albumArtwork" src="https://lh3.googleusercontent.com/proxy/fVr-noQ5XeedHxwiECY_vvOJgzLGP2m2o54ekSrHSCIF44HWP-Chg_vHv3CYFKpY8wJv3lc68OCh8knwSTSmExOC" alt="">
                         </span>
-                        <span class="artistName">
-                            <span>Drake</span>
-                        </span>
+
+                        <div class="trackInfo">
+                            <span class="trackName">
+                                <span>Forever</span>
+                            </span>
+                            <span class="artistName">
+                                <span>Drake</span>
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="nowPlayingCenter">
-                <div class="content playerControls">
-                    <div class="buttons">
-                        <button class="controlButton shuffle" title="Shuffle button" type="">
-                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                        </button>
-                        <button class="controlButton previous" title="Previous button" type="">
-                            <img src="assets/images/icons/previous.png" alt="Previous">
-                        </button>
-                        <button class="controlButton play" title="Play button" type="">
-                            <img src="assets/images/icons/play.png" alt="Play">
-                        </button>
-                        <button class="controlButton pause" title="Pause button" type="" style="display: none;">
-                            <img src="assets/images/icons/pause.png" alt="Pause">
-                        </button>
-                        <button class="controlButton next" title="Next button" type="">
-                            <img src="assets/images/icons/next.png" alt="Next">
-                        </button>
-                        <button class="controlButton repeat" title="Repeat button" type="">
-                            <img src="assets/images/icons/repeat.png" alt="Repeat">
-                        </button>
+                <div id="nowPlayingCenter">
+                    <div class="content playerControls">
+                        <div class="buttons">
+                            <button class="controlButton shuffle" title="Shuffle button" type="">
+                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                            </button>
+                            <button class="controlButton previous" title="Previous button" type="">
+                                <img src="assets/images/icons/previous.png" alt="Previous">
+                            </button>
+                            <button class="controlButton play" title="Play button" type="">
+                                <img src="assets/images/icons/play.png" alt="Play">
+                            </button>
+                            <button class="controlButton pause" title="Pause button" type="" style="display: none;">
+                                <img src="assets/images/icons/pause.png" alt="Pause">
+                            </button>
+                            <button class="controlButton next" title="Next button" type="">
+                                <img src="assets/images/icons/next.png" alt="Next">
+                            </button>
+                            <button class="controlButton repeat" title="Repeat button" type="">
+                                <img src="assets/images/icons/repeat.png" alt="Repeat">
+                            </button>
+                        </div>
+                        <div class="playbackBar">
+                            <span class="progressTime current">0.00</span>
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+                            <span class="progressTime remaining">0.00</span>
+                        </div>
                     </div>
-                    <div class="playbackBar">
-                        <span class="progressTime current">0.00</span>
+
+                </div>
+                <div id="nowPlayingRight">
+                    <div class="volumeBar">
+                        <button class="controlButton volume" title="Volume Button">
+                            <img src="assets/images/icons/volume.png" alt="Volume">
+                        </button>
+
                         <div class="progressBar">
                             <div class="progressBarBg">
                                 <div class="progress"></div>
                             </div>
-                        </div>
-                        <span class="progressTime remaining">0.00</span>
-                    </div>
-                </div>
-
-            </div>
-            <div id="nowPlayingRight">
-                <div class="volumeBar">
-                    <button class="controlButton volume" title="Volume Button">
-                        <img src="assets/images/icons/volume.png" alt="Volume">
-                    </button>
-
-                    <div class="progressBar">
-                        <div class="progressBarBg">
-                            <div class="progress"></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 
 </body>
 
