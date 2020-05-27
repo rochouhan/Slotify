3efd56c49df8dacb05f3748c59a74585396cf33d
rochouhan <ro.chouhan@gmail.com>
2020-05-27 14:27:38 -0500

    49. Volume Bar

--- index.php
+++ index.php
@@ -10,10 +10,78 @@ if (isset($_SESSION['userLoggedIn'])) {
 
 <head>
     <title>Welcome to Slotify!</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
-    Hello!
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+                <div class="content playerControls">
+                    <span class="albumLink">
+                        <img class="albumArtwork" src="https://lh3.googleusercontent.com/proxy/fVr-noQ5XeedHxwiECY_vvOJgzLGP2m2o54ekSrHSCIF44HWP-Chg_vHv3CYFKpY8wJv3lc68OCh8knwSTSmExOC" alt="">
+                    </span>
+
+                    <div class="trackInfo">
+                        <span class="trackName">
+                            <span>Forever</span>
+                        </span>
+                        <span class="artistName">
+                            <span>Drake</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle" title="Shuffle button" type="">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton previous" title="Previous button" type="">
+                            <img src="assets/images/icons/previous.png" alt="Previous">
+                        </button>
+                        <button class="controlButton play" title="Play button" type="">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </button>
+                        <button class="controlButton pause" title="Pause button" type="" style="display: none;">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+                        <button class="controlButton next" title="Next button" type="">
+                            <img src="assets/images/icons/next.png" alt="Next">
+                        </button>
+                        <button class="controlButton repeat" title="Repeat button" type="">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+                    </div>
+                    <div class="playbackBar">
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <span class="progressTime remaining">0.00</span>
+                    </div>
+                </div>
+
+            </div>
+            <div id="nowPlayingRight">
+                <div class="volumeBar">
+                    <button class="controlButton volume" title="Volume Button">
+                        <img src="assets/images/icons/volume.png" alt="Volume">
+                    </button>
+
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
