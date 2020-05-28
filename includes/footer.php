9b4514eb3bdc939f01bf21abb787cafc034c193b
rochouhan <ro.chouhan@gmail.com>
2020-05-27 20:25:44 -0500

    68. Making albums clickable links

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+</div>
+</div>
+</div>
+<div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+        <div id="nowPlayingLeft">
+            <div class="content playerControls">
+                <span class="albumLink">
+                    <img class="albumArtwork" src="assets/images/artwork/energy.jpg" alt="">
+                </span>
+
+                <div class="trackInfo">
+                    <span class="trackName">
+                        <span>Forever</span>
+                    </span>
+                    <span class="artistName">
+                        <span>Drake</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <?php include("includes/nowPlayingBar.php"); ?>
+        <div id="nowPlayingRight">
+            <div class="volumeBar">
+                <button class="controlButton volume" title="Volume Button">
+                    <img src="assets/images/icons/volume.png" alt="Volume">
+                </button>
+
+                <div class="progressBar">
+                    <div class="progressBarBg">
+                        <div class="progress"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+</body>
+
+</html>
---
+++ includes/nowPlayingBar.php
@@ -0,0 +1,34 @@
+<div id="nowPlayingCenter">
+    <div class="content playerControls">
+        <div class="buttons">
+            <button class="controlButton shuffle" title="Shuffle button" type="">
+                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+            </button>
+            <button class="controlButton previous" title="Previous button" type="">
+                <img src="assets/images/icons/previous.png" alt="Previous">
+            </button>
+            <button class="controlButton play" title="Play button" type="">
+                <img src="assets/images/icons/play.png" alt="Play">
+            </button>
+            <button class="controlButton pause" title="Pause button" type="" style="display: none;">
+                <img src="assets/images/icons/pause.png" alt="Pause">
+            </button>
+            <button class="controlButton next" title="Next button" type="">
+                <img src="assets/images/icons/next.png" alt="Next">
+            </button>
+            <button class="controlButton repeat" title="Repeat button" type="">
+                <img src="assets/images/icons/repeat.png" alt="Repeat">
+            </button>
+        </div>
+        <div class="playbackBar">
+            <span class="progressTime current">0.00</span>
+            <div class="progressBar">
+                <div class="progressBarBg">
+                    <div class="progress"></div>
+                </div>
+            </div>
+            <span class="progressTime remaining">0.00</span>
+        </div>
+    </div>
+
+</div>
